263654f950d4f773e0b98bc3b696f09152dd9131
daiki239 <ghiuuv@gmail.com>
2022-07-26 11:16:57 +0900

    6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,8 @@
  <meta charset="utf-8">
  <title>Todo List</title>
 <link rel="stylesheet" href="/css/style.css">
-
-
-
-
-
 </head>
 <body>
-
-
 <div class="list">
   <div class="card">
  <h3>Todo List</h3>
@@ -22,36 +15,28 @@
    <input name="text" placeholder="" class="input-add">
    <button class="button-add">追加</button>
  </form>
- <table>
+
+<tbody>
+  <table>   
 <tr>
             <th>作成日</th>
             <th>タスク名</th>
             <th>更新</th>
             <th>削除</th>
           </tr>
-          </table>
- 
-
-
+    </table>  
+<div class=table2>
+<td>
   @foreach ($todos as $todo)
-   
-     
    <div class="time"> {{ $todo->created_at }}</td>
-   
      <form action="/edit/{{ $todo->id }}" style="display: inline;" method="POST">        
          <input type="text" class="input-update" name="text" value="{{$todo->text}}">
               @csrf
-
-
-              
        <button class="button-update">更新</button>
      </form>
-   
    @endforeach
-
-
-
-
+</td>
+<td>
    @foreach ($todos as $todo)  
      <form action="/todos/{{ $todo->id }}"
            style="display: inline;" 
@@ -60,10 +45,11 @@
        @method('DELETE')
        <button class="button-delete">削除</button>
      </form>
-   
    @endforeach
- </ul>
-
+</td>
+</div>
+</tbody>
+   
 </div>
  </div>
  </div>
